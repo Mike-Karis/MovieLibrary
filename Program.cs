d64dd920688f095c37835d135c568a399e9eb810
Michael karis <mkaris1@my.wctc.edu>
2021-09-19 16:36:53 -0500

    search for duplicates prototype

--- Program.cs
+++ Program.cs
@@ -8,7 +8,27 @@ namespace Movie_Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines;
+            List<string> movieID = new List<string>();
+            List<string> title = new List<string>();
+            List<string> genres = new List<string>();
+
+
+
+            using (StreamReader sr = new StreamReader(File.OpenRead("movies.csv")))
+        {
+
+            while (sr.EndOfStream == false)
+            {
+            string line = sr.ReadLine();
+            lines = line.Split(',');
+            movieID.Add(lines[0]);
+            title.Add(lines[1]);
+            genres.Add(lines[2]);
+
+             
+            }
+        }
         }
     }
 }
